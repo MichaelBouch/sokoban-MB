329817eeed7aaba8f2ab48bb3c51849c7bf13eab
Michael Boduch <mibo747@gmail.com>
2018-01-17 23:17:59 +0000

    Changes after assessment

--- src/main/java/com/mb/tiles/Floor.java
+++ src/main/java/com/mb/tiles/Floor.java
@@ -1,27 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mb.tiles;
 
-/**
- *
- * @author user
- */
 public class Floor {
-    
-      private boolean isEmpty;
-      
-      
-void putOnTheFloor ()
-{
-}
-    
-    boolean isEmpty()
-    {
+
+    private boolean isEmpty;
+
+    void putOnTheFloor() {
+    }
+
+    boolean isEmpty() {
         return this.isEmpty;
     }
 }
-
-
