b0fe23d959667d6b47007c618cbda6dc08d9ac52
Michael Boduch <mibo747@gmail.com>
2018-01-17 23:49:18 +0000

    Changes after assessment

--- src/main/java/com/mb/tiles/Floor.java
+++ src/main/java/com/mb/tiles/Floor.java
@@ -1,11 +1,18 @@
 package com.mb.tiles;
 
 public class Floor {
-
+    // variables available in whole program to define type of tiles
+    public static final int FLOOR = 0;
+    public static final int WALL = 1;
+    public static final int BOX = 2;
+    public static final int PLAYER = 3;
+    public static final int SOCKET = 4;
     private Box movable;
+    private boolean isEmpty;
 
     private int tileType;
 
+     
     public Box getMovable() {
         return movable;
     }
@@ -24,11 +31,11 @@ public class Floor {
 
     
 
-    Box isEmpty() {
+    boolean isEmpty() {
         if (movable == null) {
-            return null;
+            return false;
         } else {
-            return movable;
+            return true;
         }
     }
 }
